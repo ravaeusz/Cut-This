fd846c4ddd5faea67ee2ca34c3a4888a7c152c47
Rafael Girardi <girardirafael2003@gmail.com>
2025-07-01 23:17:42 -0300

    Delete Cut This directory

--- Cut This/frontend/src/components/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="w-full h-[100px] bg-red-500 mt-auto flex items-center justify-center">
-      <h1 className="text-white">FOOTER</h1>
-    </footer>
-  );
-}
